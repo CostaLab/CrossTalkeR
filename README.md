49dcd2feb46e9d1a788633cf8445ea97283d3a30
James Nagai <james.nagai@gmail.com>
2021-01-18 13:37:27 +0100

    small updates: readme and templates

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # CrossTalker
+
 [![DOI](https://zenodo.org/badge/329572127.svg)](https://zenodo.org/badge/latestdoi/329572127)
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/CostaLab/CrossTalkeR.js/graphs/commit-activity)
+[![GitHub license](https://img.shields.io/github/license/CostaLab/CrossTalkeR.svg)](https://github.com/CostaLab/CrossTalkeR.js/blob/master/LICENSE)
+[![GitHub release](https://img.shields.io/github/release/CostaLab/CrossTalkeR.svg)](https://GitHub.com/CostaLab/CrossTalkeR.js/releases/)
+
 
 
 James S. Nagai<sup>1</sup>,
 Nils B. Leimkühler<sup>2</sup>,
 Rebekka K. Schneider<sup>2,3,4</sup>,
 Ivan G. Costa<sup>1*</sup>
-
-
 
 <sup>1</sup>Institute for Computational Genomics, Faculty of Medicine, RWTH Aachen University, Aachen, 52074 Germany
 
@@ -17,8 +20,16 @@ Ivan G. Costa<sup>1*</sup>
 
 <sup>4</sup>Oncode Institute, Erasmus Medical Center, Rotterdam, 3015GD, the Netherlands
 
+![scheme](./CrossTalkeR_only_A.png)
 
 
+
+
+```{r}
+install.packages("devtools")
+devtools::install_github("https://github.com/CostaLab/CrossTalkeR%22)
+require(CrossTalkeR)
+```
 
 CrossTalkNet aims to provide a user friendly way to visualize cellular crosstalk through ligand receptor networks. It use as input the Ligand Receptor Inference Tool Results to build a sharable report with main network based analysis
 
@@ -31,7 +42,7 @@ CrossTalkNet aims to provide a user friendly way to visualize cellular crosstalk
    - CCI and GCI PCA ranking
       - All measures and PC table
       - PC1 and PC2 based barplot
-   - Leimkühler data were added to the package
+   - Leimkühler et. al. [1] data were added to the package
 
 
 
---
+++ doc/CrossTalkeR.R
@@ -0,0 +1,46 @@
+## ----vignete0-----------------------------------------------------------------
+suppressPackageStartupMessages({require(CrossTalkeR)})
+
+## ----vignete1, results="hide", warning=FALSE,message=F, output=FALSE----------
+paths <- c('CTR' = system.file("extdata",
+                               "ctr_nils_bm_human.csv",
+                               package = "CrossTalkeR"),
+           'EXP' = system.file("extdata",
+                               "exp_nils_bm_human.csv",
+                               package = "CrossTalkeR"))
+genes <- c('TGFB1','PF4','PPBP')
+data <- generate_report(paths,
+                        genes,
+                        out_path='~/Documents/',
+                        threshold=0,
+                        out_file = 'vignettes_example.html',
+                        output_fmt = "html_document")
+
+## ----vignete2, results="hide", warning=FALSE,message=F, output=FALSE,fig.width=8,fig.height=8----
+plot_cci(graph = data@graphs$CTR,
+        colors = data@colors,
+        plt_name = 'Example 1',
+        coords = data@coords[V(data@graphs$CTR)$name,],
+        emax = NULL,
+        leg = FALSE,
+        low = 0,
+        high = 0,
+        ignore_alpha = FALSE,
+        log = FALSE,
+        efactor = 8,
+        vfactor = 12)
+
+
+## ----vignete3, results="hide", warning=FALSE,message=F, output=FALSE,fig.width=15,fig.height=15----
+plot_ggi(graph = data@graphs_ggi$EXP_x_CTR,
+         color = data@colors)
+
+
+## ----vignete4, results="hide", warning=FALSE,message=F, output=FALSE,fig.width=15,fig.height=10----
+plot_sankey(lrobj_tbl = data@tables$EXP_x_CTR,
+            target = c("TGFB1"),
+            ligand_cluster = NULL,
+            receptor_cluster = NULL,
+            plt_name = "TGFB1")
+
+
